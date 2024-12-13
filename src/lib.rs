3c676ef5cee053b1bb9b9f034df6bf146491e761
sigurd4 <sigurd.spangelo@gmail.com>
2024-12-13 17:33:50 +0100

    Fix formatting

--- src/lib.rs
+++ src/lib.rs
@@ -1,25 +1,40 @@
 #![cfg_attr(not(test), no_std)]
-
 #![feature(trait_alias)]
 #![feature(const_trait_impl)]
 
-use core::ops::{Index, Range, RangeInclusive, RangeFrom, RangeTo, RangeToInclusive, RangeFull, IndexMut};
+use core::ops::{
+    Index, IndexMut, Range, RangeFrom, RangeFull, RangeInclusive, RangeTo, RangeToInclusive,
+};
 
 pub trait SlicePrereq<T> = ?Sized
-+ /*~const*/ Index<usize, Output = <[T] as Index<usize>>::Output>
-+ /*~const*/ Index<Range<usize>, Output = <[T] as Index<Range<usize>>>::Output>
-+ /*~const*/ Index<RangeInclusive<usize>, Output = <[T] as Index<RangeInclusive<usize>>>::Output>
-+ /*~const*/ Index<RangeFrom<usize>, Output = <[T] as Index<RangeFrom<usize>>>::Output>
-+ /*~const*/ Index<RangeTo<usize>, Output = <[T] as Index<RangeTo<usize>>>::Output>
-+ /*~const*/ Index<RangeToInclusive<usize>, Output = <[T] as Index<RangeToInclusive<usize>>>::Output>
-+ /*~const*/ Index<RangeFull, Output = <[T] as Index<RangeFull>>::Output>
-+ /*~const*/ IndexMut<usize>
-+ /*~const*/ IndexMut<Range<usize>>
-+ /*~const*/ IndexMut<RangeInclusive<usize>>
-+ /*~const*/ IndexMut<RangeFrom<usize>>
-+ /*~const*/ IndexMut<RangeTo<usize>>
-+ /*~const*/ IndexMut<RangeToInclusive<usize>>
-+ /*~const*/ IndexMut<RangeFull>;
+    + /*~const*/
+    Index<usize, Output = <[T] as Index<usize>>::Output>
+    + /*~const*/
+    Index<Range<usize>, Output = <[T] as Index<Range<usize>>>::Output>
+    + /*~const*/
+    Index<RangeInclusive<usize>, Output = <[T] as Index<RangeInclusive<usize>>>::Output>
+    + /*~const*/
+    Index<RangeFrom<usize>, Output = <[T] as Index<RangeFrom<usize>>>::Output>
+    + /*~const*/
+    Index<RangeTo<usize>, Output = <[T] as Index<RangeTo<usize>>>::Output>
+    + /*~const*/
+    Index<RangeToInclusive<usize>, Output = <[T] as Index<RangeToInclusive<usize>>>::Output>
+    + /*~const*/
+    Index<RangeFull, Output = <[T] as Index<RangeFull>>::Output>
+    + /*~const*/
+    IndexMut<usize>
+    + /*~const*/
+    IndexMut<Range<usize>>
+    + /*~const*/
+    IndexMut<RangeInclusive<usize>>
+    + /*~const*/
+    IndexMut<RangeFrom<usize>>
+    + /*~const*/
+    IndexMut<RangeTo<usize>>
+    + /*~const*/
+    IndexMut<RangeToInclusive<usize>>
+    + /*~const*/
+    IndexMut<RangeFull>;
 
 #[const_trait]
 pub trait Slice: private::Slice + SlicePrereq<<Self as Slice>::Item>
@@ -36,7 +51,7 @@ pub trait Slice: private::Slice + SlicePrereq<<Self as Slice>::Item>
 impl<T> const Slice for [T]
 {
     type Item = T;
-    
+
     fn as_slice(&self) -> &[Self::Item]
     {
         self
@@ -64,10 +79,9 @@ mod test
 
         fn first<'a, S: Slice + ?Sized>(slice: &'a S) -> Option<&'a S::Item>
         where
-            S::Item: Copy
+            S::Item: Copy,
         {
-            slice.as_slice()
-                .first()
+            slice.as_slice().first()
         }
 
         println!("{:?}", first(a_slice))
@@ -77,6 +91,6 @@ mod test
 mod private
 {
     pub trait Slice {}
-    
+
     impl<T> Slice for [T] {}
 }
