0f784a7b19f59f99b395794f3648d88401fdc4ae
Sigurd Sætherø Spangelo <sigurd dot spangelo at gmail dot com>
2025-10-09 22:24:47 +0200

    Version 0.2.3

--- src/slice.rs
+++ src/slice.rs
@@ -1,8 +1,10 @@
+use core::ptr::Pointee;
+
 use crate::{private, SlicePrereq, AsSlice};
 
 /// A trait for a slice `[Self::Elem]`
 #[const_trait]
-pub trait Slice: private::Slice + SlicePrereq<<Self as AsSlice>::Elem> + ~const AsSlice
+pub trait Slice: private::Slice + SlicePrereq<<Self as AsSlice>::Elem> + ~const AsSlice + Pointee<Metadata = usize>
 {
     
 }
